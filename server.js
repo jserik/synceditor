1d9daf91aa43c929c7547dbe4ce5de678be4a312
Erik <mail@eriks.one>
2022-01-12 17:20:44 +0100

    added

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-express
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 4000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+// Specify the url prefix and import routes
+app.use('/', require('./routes'));
+
+app.listen(port, () => {
+  console.log(`Success! Your application is running on port ${port}.`);
+});
